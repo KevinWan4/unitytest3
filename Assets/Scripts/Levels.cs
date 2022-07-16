4691edf557a47eaae09cd9d3873661e1a3b91772
Samin325 <samin.zaman325@gmail.com>
2022-07-15 20:41:33 -0600

    Create Level class

---
+++ Assets/Scripts/Levels.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Levels : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void endLevel()
+    {
+
+    }
+
+    void newScene((reference tilegrid), int level)
+    {
+
+    }
+
+
+
+    // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+}
